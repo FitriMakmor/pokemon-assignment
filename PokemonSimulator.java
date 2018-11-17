24b703427f568438216b1bbbafed33f5e6435989
Fitri Makmor <44615165+FitriMakmor@users.noreply.github.com>
2018-11-17 10:04:48 +0800

    Updated Moveset
    Change the String values to include Space, because I forgot that a string can have space 😌

--- PokemonSimulator.java
+++ PokemonSimulator.java
@@ -18,38 +18,38 @@ public static void Moveset(int pokeslot1, int pokeslot2, int pokemon1[][], int p
     double dmg;
     switch(skill[pokeslot1][pokeslot1])
     {
-        case "takeDown":        dmg=damage(85-accuracy1,pokemon1[pokeslot1][0],90,pokemon2[pokeslot2][1],0);
+        case "Take Down":        dmg=damage(85-accuracy1,pokemon1[pokeslot1][0],90,pokemon2[pokeslot2][1],0);
                                 pokemon2[pokeslot2][2]-=dmg;
                                 pokemon1[pokeslot1][2]-=dmg/4;
                                 break;
-        case "quickAttack":     speed+=50;
+        case "Quick Attack":     speed+=50;
                                 dmg=damage(100-accuracy1,pokemon1[pokeslot1][0],40,pokemon2[pokeslot2][1],0);
                                 pokemon2[pokeslot2][2]-=dmg;
                                 break;
-        case "tailWhip":        pokemon2[pokeslot2][1]-=20;
+        case "Tail Whip":        pokemon2[pokeslot2][1]-=20;
                                 if(pokemon2[pokeslot2][1]<0)
                                     pokemon2[pokeslot2][1]=0;
-        case "sandAttack":
-        case "flash":           accuracy2-=15;
+        case "Sand Attack":
+        case "Flash":           accuracy2-=15;
                                 break;
-        case "hyperBeam":       speed-=100;
+        case "Hyper Beam":       speed-=100;
                                 dmg=damage(90-accuracy1,pokemon1[pokeslot1][0],150,pokemon2[pokeslot2][1],0);
                                 pokemon2[pokeslot2][2]-=dmg;
                                 break;
-        case "megaPunch":       dmg=damage(85-accuracy1,pokemon1[pokeslot1][0],80,pokemon2[pokeslot2][1],0);
+        case "Mega Punch":       dmg=damage(85-accuracy1,pokemon1[pokeslot1][0],80,pokemon2[pokeslot2][1],0);
                                 pokemon2[pokeslot2][2]-=dmg;
                                 break;
-        case "rest":            speed-=200;
+        case "Rest":            speed-=200;
                                 pokemon1[pokeslot1][2]=160;//Only for Snorlax for now
                                 break;
-        case "harden":          pokemon1[pokeslot1][1]+=15;
+        case "Harden":          pokemon1[pokeslot1][1]+=15;
                                 break;
-        case "tackle":          dmg=damage(100-accuracy1,pokemon1[pokeslot1][0],40,pokemon2[pokeslot2][1],0);
+        case "Tackle":          dmg=damage(100-accuracy1,pokemon1[pokeslot1][0],40,pokemon2[pokeslot2][1],0);
                                 pokemon2[pokeslot2][2]-=dmg;
                                 break;
-        case "recover":         pokemon1[pokeslot1][2]+=32.50;//Only for Porygon for now
+        case "Recover":         pokemon1[pokeslot1][2]+=32.50;//Only for Porygon for now
                                 break;
-        case "agility":         pokemon1[pokeslot1][3]+=15;
+        case "Agility":         pokemon1[pokeslot1][3]+=15;
                                 break;
         
     }
