ce37018f250cc93cff985902d38de02601656468
jinghong1213 <45033500+jinghong1213@users.noreply.github.com>
2018-11-20 17:27:16 +0800

    Create Starting of Game.java

---
+++ Starting of Game.java
@@ -0,0 +1,23 @@
+package pokemon.simulator;
+
+/**
+ *
+ * @author Jinghong
+ */
+public class PokemonSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String
+        System.out.println("\n\n\n\n\n\t\t");
+        
+        System.out.println("Hi Trainer, Welcome to Pokemon World!");
+        
+        System.out.println("What should I call you ? ");
+        
+        name=s.nextLine();
+    }
