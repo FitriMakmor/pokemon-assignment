e5a6e47515455d20b7a487650b73d7f44fefdc69
jinghong1213 <45033500+jinghong1213@users.noreply.github.com>
2018-11-26 00:06:22 +0800

    Update PokemonSimulator.java

--- PokemonSimulator.java
+++ PokemonSimulator.java
@@ -22,10 +22,16 @@ public class PokemonSimulator {
         
         name=s.nextLine();
         
-        System.out.print("Hi " +name);
+        System.out.print("\nHi " +name);
         System.out.print("! Good to see you!");
         System.out.println("Your very own adventure is about to unfold. Are you ready?");
         System.out.println("Now, I'll let you to choose your very first pokemon!");
          
-        System.out.println("You have ");
+        System.out.println("\n\nWhich pokemon would you like to choose? \n1.CHARMANDER(FIRE) 2.CYNDAQUIL(FIRE) 3.BLAZIKEN(FIRE)\n"
+                 + "4.EEVEE(NORMAL) 5.SNORLAX(NORMAL) 6.PORYGON(NORMAL) \n7.SCEPTILE(GRASS) 8.GROTLE(GRASS) 9.SNIVY(GRASS)"
+                 + "\n10.SQUIRTLE(WATER) 11.TOTODILE(WATER) 12.MUDKIP(WATER)");
+                 
+       String 
+          
+
     }
