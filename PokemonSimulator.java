b546c6127aa98d24b4e6160dcb0e6830d9725245
jinghong1213 <45033500+jinghong1213@users.noreply.github.com>
2018-11-28 03:17:42 +0800

    Update PokemonSimulator.java

--- PokemonSimulator.java
+++ PokemonSimulator.java
@@ -22,16 +22,98 @@ public class PokemonSimulator {
         
         name=s.nextLine();
         
-        System.out.print("\nHi " +name);
-        System.out.print("! Good to see you!");
-        System.out.println("Your very own adventure is about to unfold. Are you ready?");
-        System.out.println("Now, I'll let you to choose your very first pokemon!");
+         System.out.print("\nHi " +name);
+         System.out.print("! Good to see you!");
+         System.out.println("Your very own adventure is about to unfold. Are you ready?");
+         System.out.println("Now, I'll let you to choose your very first pokemon!");
          
-        System.out.println("\n\nWhich pokemon would you like to choose? \n1.CHARMANDER(FIRE) 2.CYNDAQUIL(FIRE) 3.BLAZIKEN(FIRE)\n"
+         System.out.println("\nWhich pokemon would you like to choose?Please type the full name. \n1.CHARMANDER(FIRE) 2.CYNDAQUIL(FIRE) 3.BLAZIKEN(FIRE)\n"
                  + "4.EEVEE(NORMAL) 5.SNORLAX(NORMAL) 6.PORYGON(NORMAL) \n7.SCEPTILE(GRASS) 8.GROTLE(GRASS) 9.SNIVY(GRASS)"
                  + "\n10.SQUIRTLE(WATER) 11.TOTODILE(WATER) 12.MUDKIP(WATER)");
-                 
-       String 
+        
+
+        String charmander = "charmander",cyndaquil="cyndaquil",blaziken="blaziken",eevee="eevee";
+        String snorlax="snorlax",porygon="porygon",sceptile="sceptile",grotle="grotle";
+        String snivy="snivy",squirtle="squirtle",totodile="totodile",mudkip="mudkip";
+      
+        String answer=s.nextLine();
+        
+        
+       
+        while(answer .equals(charmander)||answer.equals(cyndaquil)||answer.equals(blaziken)||answer.equals(eevee)||answer.equals(snorlax)||answer.equals(porygon)||answer.equals(sceptile)||answer.equals(grotle)||answer.equals(snivy)||answer.equals(squirtle)||answer.equals(totodile)||answer.equals(mudkip))
+      {
+        if(answer .equals(charmander))
+        {
+        System.out.println("Okay,so you've chose "+charmander+ " as your first pokemon.");
+        break;
+        } 
+        
+        else if(answer .equals(cyndaquil))
+        {
+        System.out.println("Okay,so you've chose "+cyndaquil+ " as your first pokemon.");
+        break;
+        }
+        
+        else if(answer .equals(blaziken))
+        {
+        System.out.println("Okay,so you've chose "+blaziken+ " as your first pokemon.");
+        break;
+        }
+        
+        else if(answer .equals(eevee))
+        {
+        System.out.println("Okay,so you've chose "+eevee+ " as your first pokemon.");
+        break;
+        }
+        
+        else if(answer .equals(snorlax))
+        {
+        System.out.println("Okay,so you've chose "+snorlax+ " as your first pokemon.");
+        break;
+        }
+        
+        else if(answer .equals(porygon))
+        {
+        System.out.println("Okay,so you've chose "+porygon+ " as your first pokemon.");
+        break;
+        }
+        else if(answer .equals(sceptile))
+        {
+        System.out.println("Okay,so you've chose "+sceptile+ " as your first pokemon.");
+        break;
+        }
+        else if(answer .equals(grotle))
+        {
+        System.out.println("Okay,so you've chose "+grotle+ " as your first pokemon.");
+        break;
+        }
+        else if(answer .equals(snivy))
+        {
+        System.out.println("Okay,so you've chose "+snivy+ " as your first pokemon.");
+        break;
+        }
+        else if(answer .equals(squirtle))
+        {
+        System.out.println("Okay,so you've chose "+squirtle+ " as your first pokemon.");
+        break;
+        }
+        else if(answer .equals(totodile))
+        {
+        System.out.println("Okay,so you've chose "+totodile+ " as your first pokemon.");
+        break;
+        }
+        else if(answer .equals(mudkip))
+        {
+        System.out.println("Okay,so you've chose "+mudkip+ " as your first pokemon.");
+        break;
+        }
+        else
+        {
+        System.out.println("What ? Wrong pokemon name.Please type again.");
+        answer=s.nextLine();
+        }
+      }
+        
           
 
     }
